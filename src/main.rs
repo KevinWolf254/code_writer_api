65aa17c08753e6ad84ce14ab9f0c7bd2ab5a2d48
kkanyi <kkanyi@co-opbank.co.ke>
2023-09-06 12:13:03 +0300

    01-struct-definitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u32,
+    name: String,
+    completed: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u32,
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u32, Task>,
+    users: HashMap<u32, User>
+}
+
 fn main() {
     println!("Hello, world!");
 }
